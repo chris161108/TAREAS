9bdbfc57a8488d7b951997d67206fd8c5d0f2502
chris161108 <christopher.gonzalezlopezz@gmail.com>
2026-06-05 13:59:21 -0400

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,7 @@
+let edadUsuario = 20 
+if (edadUsuario >= 18) {
+    console.log("PUEDES TENER TU LICENCIA DE CONDUCIR");
+} 
+else {
+    console.log("Eres menor de edad, no puedes tener tu licencia de conducir");
+}
---
+++ 4.js
@@ -0,0 +1,13 @@
+let colorSemaforo = "verde";
+if (colorSemaforo === "verde") {
+    console.log("Puedes pasar");
+}
+else if (colorSemaforo === "amarillo") {
+    console.log("Reduzca la velocidad, precaucion.");
+}
+else if (colorSemaforo === "rojo") {
+    console.log("debes detenerte");
+}
+else {
+    console.log("Color de semáforo no valido");
+}
