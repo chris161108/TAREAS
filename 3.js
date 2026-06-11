180493c2e1e61c5423a81d00c6c226e036e3e210
chris161108 <christopher.gonzalezlopezz@gmail.com>
2026-06-10 23:42:30 -0400

    Delete 3.js

--- 3.js
+++
@@ -1,7 +0,0 @@
-let edadUsuario = 20 
-if (edadUsuario >= 18) {
-    console.log("PUEDES TENER TU LICENCIA DE CONDUCIR");
-} 
-else {
-    console.log("Eres menor de edad, no puedes tener tu licencia de conducir");
-}
