65e1ef5f3b73b6a7fe0036c264b84a74cf220342
chris161108 <christopher.gonzalezlopezz@gmail.com>
2026-06-10 23:42:07 -0400

    Delete 1.js

--- 1.js
+++
@@ -1,9 +0,0 @@
-const nombreproducto = "Tablet 10 pulgadas";
-let precio = "450.99"
-let cantidad_stock = 25
-const envio_gratis = true
-
-console.log("Nombre del producto: " , nombreproducto);
-console.log("Precio: " , precio);
-console.log("Cantidad en stock: " , cantidad_stock);
-console.log("Envío gratis: " , envio_gratis);
