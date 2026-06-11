7b676402778efcdc3ed23f8ef4014ba954cdee93
chris161108 <christopher.gonzalezlopezz@gmail.com>
2026-06-10 23:41:50 -0400

    Add files via upload

---
+++ tareas-javascript/5.js
@@ -0,0 +1,24 @@
+let diaSemana = 3
+switch (diaSemana) {
+    case 1:
+        console.log("Lunes: lentajas");
+        break;
+    case 2:
+        console.log("Martes: pollo al horno");
+        break;
+    case 3:
+        console.log("Miercoles: pezcado a la plancha");       
+        break;
+    case 4:
+        console.log("Jueves: pasta ");
+        break;
+    case 5:
+        console.log("Viernes: paella");
+        break;
+
+    default:
+        console.log("Día no válido para menu");
+        
+
+
+}
